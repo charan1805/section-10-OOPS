a70cdbb2d1b2e44c3d40ca2a58b54e4d56e075cb
charan1805 <sreecharanmudhiraj@gmail.com>
2023-08-01 20:47:44 +0530

    first class

---
+++ TeacherwithSubjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    public class TeacherWithSubjects
+    {
+        public Teacher Teacher { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public TeacherWithSubjects()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+
+}
